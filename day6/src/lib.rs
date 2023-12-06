8b478046ad4c45d1204f69b0824c707e85fa6c36
Carl Lundin <carllundin55@gmail.com>
2023-12-05 22:11:19 -0800

    Finished day six part two.

--- day6/src/lib.rs
+++ day6/src/lib.rs
@@ -1,35 +1,77 @@
-fn calculate_possible_winning_races(time: u32, record: u32) -> u32 {
+fn calculate_possible_winning_races(time: u128, record: u128) -> u128 {
     let mut combinations = 0;
+
     for hold in 1..time {
         let remainder = time - hold;
-        let distance = hold * remainder;
-        if distance >record {
+        if hold > record / remainder {
             combinations += 1;
         }
     }
     combinations
 }
 
-pub fn boat_race(input: &str) -> u32 {
+pub fn boat_race(input: &str) -> u128 {
     let mut times = Vec::new();
     let mut distances = Vec::new();
     for line in input.lines() {
         match line {
             line if line.contains("Time") => {
-                times = line.split_whitespace().filter_map(|digit| u32::from_str_radix(digit, 10).ok()).collect();
-            },
+                times = line
+                    .split_whitespace()
+                    .filter_map(|digit| u128::from_str_radix(digit, 10).ok())
+                    .collect();
+            }
             line if line.contains("Distance") => {
-                distances = line.split_whitespace().filter_map(|digit| u32::from_str_radix(digit, 10).ok()).collect();
-            },
+                distances = line
+                    .split_whitespace()
+                    .filter_map(|digit| u128::from_str_radix(digit, 10).ok())
+                    .collect();
+            }
             _ => (),
         }
     }
     let mut race_possibilities = Vec::new();
     for (time, distance) in times.into_iter().zip(distances) {
-        race_possibilities.push(calculate_possible_winning_races(time,distance));
+        race_possibilities.push(calculate_possible_winning_races(time, distance));
     }
 
     race_possibilities.iter().product()
+}
+
+pub fn boat_race_part_two(input: &str) -> u128 {
+    let mut time: u128 = 0;
+    let mut distance: u128 = 0;
+    for line in input.lines() {
+        match line {
+            line if line.contains("Time") => {
+                for digit in line.chars().filter(|c| c.is_numeric()) {
+                    if let Some(digit) = digit.to_digit(10) {
+                        let digit: u128 = digit.into();
+                        if time == 0 {
+                            time = digit.into();
+                        } else {
+                            time = (time * 10) + digit;
+                        }
+                    }
+                }
+            }
+            line if line.contains("Distance") => {
+                for digit in line.chars().filter(|c| c.is_numeric()) {
+                    if let Some(digit) = digit.to_digit(10) {
+                        let digit: u128 = digit.into();
+                        if distance == 0 {
+                            distance = digit.into();
+                        } else {
+                            distance = (distance * 10) + digit;
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+
+    calculate_possible_winning_races(time, distance)
 }
 
 #[cfg(test)]
@@ -46,8 +88,20 @@ mod tests {
     }
 
     #[test]
+    fn sample_boat_race_part_two() {
+        let result = boat_race_part_two(SAMPLE);
+        assert_eq!(result, 71503);
+    }
+
+    #[test]
     fn input_boat_race() {
         let result = boat_race(INPUT);
-        assert_eq!(result, 288);
+        assert_eq!(result, 1413720);
+    }
+
+    #[test]
+    fn input_boat_race_part_two() {
+        let result = boat_race_part_two(INPUT);
+        assert_eq!(result, 30565288);
     }
 }
